6321bff8d2c8136686e7a5255106a370c3abb0da
Scott Powell <spowell@adkgroup.com>
2024-05-08 22:51:08 -0400

    move templates

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/scottapow/scottapow/pages"
+
 	"github.com/a-h/templ"
 )
 
 func main() {
-	component := home()
-
-	http.Handle("/", templ.Handler(component))
+	http.Handle("/", templ.Handler(pages.Home()))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
