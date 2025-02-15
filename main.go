fe8ff4ee6f4f206ab80c82cf54f5ad8e472e689c
Scott Powell <lfh.scot@gmail.com>
2025-02-14 21:47:39 -0500

    remove error checking on loading env as this is only used in local dev

--- main.go
+++ main.go
@@ -14,10 +14,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	store, err := db.Connect()
 	if err != nil {
