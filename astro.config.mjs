99c81fdcddafd73886f82bed418c46b8b86414ab
Scott Powell <lfh.scot@gmail.com>
2025-09-08 22:02:12 -0400

    fix port

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: 'standalone',
+  }),
+  server: {
+    port: 3000,
+  }
 });
